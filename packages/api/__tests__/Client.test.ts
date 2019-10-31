7fcf8c3bfbb18890e4c6f27cb83dc3f3a2526106
Bhaskar Murthy <bhaskar@secretmission.io>
2019-10-31 14:43:56 -0400

    Use mock axios response for unit tests

--- packages/api/__tests__/Client.test.ts
+++ packages/api/__tests__/Client.test.ts
@@ -1,4 +1,8 @@
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
 import Client from '../src/Client'
+
+const mock = new MockAdapter(axios) // set mock adapter on default axios instance
 
 describe('@freshbooks/api', () => {
 	describe('Client', () => {
@@ -9,8 +13,16 @@ describe('@freshbooks/api', () => {
 		})
 
 		test('Test unauthorized request', async () => {
+			mock
+				.onGet('/auth/api/v1/users/me')
+				.replyOnce(
+					401,
+					'{"error":"unauthenticated","error_description":"This action requires authentication to continue."}'
+				)
+
 			const client = new Client('foo')
 			const res = await client.users.me()
+
 			expect(res.ok).toBeFalsy()
 			expect(res.error).not.toBeUndefined()
 
@@ -20,9 +32,15 @@ describe('@freshbooks/api', () => {
 		})
 
 		test('Test pagination', async () => {
-			const client = new Client(
-				'af29ed8f8d5d3399791212ce5be512018214b7aebc8d942744a8a45e8177919a'
-			)
+			const accountId = 'xZNQ1X'
+			mock
+				.onGet(`/accounting/account/${accountId}/invoices/invoices`)
+				.replyOnce(
+					200,
+					'{"response":{"result":{"invoices": [],"page": 1,"pages": 1,"per_page": 15,"total": 7}}}'
+				)
+
+			const client = new Client('foo')
 			const res = await client.invoices.list('xZNQ1X')
 			expect(res.ok).toBeTruthy()
 			expect(res.data).not.toBeUndefined()
