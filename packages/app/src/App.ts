98f6a03b8eff59d70d487dde0d244642780c28f5
Bhaskar Murthy <bhaskar@secretmission.io>
2019-11-01 11:15:19 -0400

    Add OAuth2 strategy

--- packages/app/src/App.ts
+++ packages/app/src/App.ts
@@ -1,9 +1,36 @@
 import express from 'express'
+import passport from 'passport'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import OAuth2Strategy from 'passport-oauth2'
+import FreshbooksStrategy from './PassportStrategy'
 
-const app = express()
-app.use(helmet())
-app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'common'))
+export default function(
+	clientId: string,
+	clientSecret: string,
+	callbackURL: string,
+	verify: OAuth2Strategy.VerifyFunction
+): express.Express {
+	const app = express()
 
-export default app
+	// init client params
+	app.set('CLIENT_ID', clientId)
+	app.set('CLIENT_SECRET', clientSecret)
+
+	// set up middleware
+	app.use(helmet())
+	app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'common'))
+
+	// set up auth
+	passport.use(
+		'freshbooks',
+		new FreshbooksStrategy(clientId, clientSecret, callbackURL, verify)
+	)
+
+	const router = express.Router()
+	router.use('/freshbooks', passport.authenticate('freshbooks'))
+
+	app.use('/auth', router)
+
+	return app
+}
---
+++ packages/app/src/PassportStrategy.ts
@@ -0,0 +1,35 @@
+import OAuth2Strategy from 'passport-oauth2'
+
+export const AUTHORIZATION_URL =
+	'https://my.freshbooks.com/service/auth/oauth/authorize'
+export const TOKEN_URL = 'https://api.freshbooks.com/auth/oauth/token'
+
+export default class FreshbooksStrategy extends OAuth2Strategy {
+	private readonly clientId: string
+
+	private readonly clientSecret: string
+
+	private readonly callbackURL: string
+
+	constructor(
+		clientId: string,
+		clientSecret: string,
+		callbackURL: string,
+		verify: OAuth2Strategy.VerifyFunction
+	) {
+		super(
+			{
+				authorizationURL: AUTHORIZATION_URL,
+				tokenURL: TOKEN_URL,
+				clientID: clientId,
+				clientSecret,
+				callbackURL,
+			},
+			verify
+		)
+
+		this.clientId = clientId
+		this.clientSecret = clientSecret
+		this.callbackURL = callbackURL
+	}
+}
