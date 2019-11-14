4d15499b93e0bba0cb538669755976b058888df2
Nedu Robert <nedumrobert@gmail.com>
2019-11-14 11:54:18 -0500

    feat: added BusinessMembership model and tests

---
+++ packages/api/__tests__/BusinessMembership.ts
@@ -0,0 +1,154 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import {
+	transformBusinessMembershipJSON,
+	transformBusinessMembershipResponse,
+} from '../src/models/BusinessMembership'
+
+describe('@freshbooks/api', () => {
+	describe('Business Membership', () => {
+		test('Verify JSON -> model transform', async () => {
+			const json = `{
+                "id": 168372,
+                "role": "owner",
+                "business": {
+                    "id": 77128,
+                    "name": "BillSpring",
+                    "account_id": "zDmNq",
+                    "address": {
+                        "id": 74595,
+                        "street": "123",
+                        "city": "Toronto",
+                        "province": "Ontario",
+                        "country": "Canada",
+                        "postal_code": "A1B2C3"
+                    },
+                    "phone_number": null,
+                    "business_clients": [
+                        {
+                            "id": 22347,
+                            "business_id": 77128,
+                            "account_id": "Xr82w",
+                            "userid": 74353,
+                            "client_business": {
+                                "business_id": 77128
+                            },
+                            "account_business": {
+                                "account_business_id": 363103,
+                                "account_id": "Xr82w"
+                            }
+                        }
+                    ]
+                }
+            }`
+			const model = transformBusinessMembershipJSON(json)
+
+			expect(model).toEqual(
+				expect.objectContaining({
+					id: '168372',
+					role: 'owner',
+					business: {
+						id: '77128',
+						name: 'BillSpring',
+						accountId: 'zDmNq',
+						address: {
+							id: '74595',
+							street: '123',
+							city: 'Toronto',
+							province: 'Ontario',
+							country: 'Canada',
+							postalCode: 'A1B2C3',
+						},
+						phoneNumber: {},
+						businessClients: [
+							{
+								id: '22347',
+								businessId: '77128',
+								accountId: 'Xr82w',
+								userId: '74353',
+								clientBusiness: {
+									businessId: '77128',
+								},
+								accountBusiness: {
+									businessId: '363103',
+									accountId: 'Xr82w',
+								},
+							},
+						],
+					},
+				})
+			)
+		})
+		test('Verify parsed JSON -> model transform', async () => {
+			const data = {
+				id: 168372,
+				role: 'owner',
+				business: {
+					id: 77128,
+					name: 'BillSpring',
+					account_id: 'zDmNq',
+					address: {
+						id: 74595,
+						street: '123',
+						city: 'Toronto',
+						province: 'Ontario',
+						country: 'Canada',
+						postal_code: 'A1B2C3',
+					},
+					phone_number: null,
+					business_clients: [
+						{
+							id: 22347,
+							business_id: 77128,
+							account_id: 'Xr82w',
+							userid: 74353,
+							client_business: {
+								business_id: 77128,
+							},
+							account_business: {
+								account_business_id: 363103,
+								account_id: 'Xr82w',
+							},
+						},
+					],
+				},
+			}
+			const model = transformBusinessMembershipResponse(data)
+
+			expect(model).toEqual(
+				expect.objectContaining({
+					id: '168372',
+					role: 'owner',
+					business: {
+						id: '77128',
+						name: 'BillSpring',
+						accountId: 'zDmNq',
+						address: {
+							id: '74595',
+							street: '123',
+							city: 'Toronto',
+							province: 'Ontario',
+							country: 'Canada',
+							postalCode: 'A1B2C3',
+						},
+						phoneNumber: {},
+						businessClients: [
+							{
+								id: '22347',
+								businessId: '77128',
+								accountId: 'Xr82w',
+								userId: '74353',
+								clientBusiness: {
+									businessId: '77128',
+								},
+								accountBusiness: {
+									businessId: '363103',
+									accountId: 'Xr82w',
+								},
+							},
+						],
+					},
+				})
+			)
+		})
+	})
+})
--- packages/api/src/models/BusinessMembership.ts
+++ packages/api/src/models/BusinessMembership.ts
@@ -1,4 +1,5 @@
 import Business from './Business'
+import { transformBusinessResponse } from './Business'
 
 export default interface BusinessMembership {
 	id: string
@@ -9,6 +10,40 @@ export default interface BusinessMembership {
 /**
  * Parse a JSON string to @BusinessMembership object
  * @param json JSON string
+ * eg: '{
+ * 			"id": 168372,
+ * 			"role": "owner",
+ * 			"business": {
+ *          	"id": 77128,
+ *          	"name": "BillSpring",
+ *          	"account_id": "zDmNq",
+ *          	"address": {
+ *              	"id": 74595,
+ *              	"street": "123",
+ *              	"city": "Toronto",
+ *              	"province": "Ontario",
+ *              	"country": "Canada",
+ *              	"postal_code": "A1B2C3"
+ *          	},
+ *          	"phone_number": null,
+ *          	"business_clients": [
+ *           		{
+ *              		"id": 22347,
+ *              		"business_id": 77128,
+ *              		"account_id": "Xr82w",
+ *              		"userid": 74353,
+ *              		"client_business": {
+ *                  		"business_id": 77128
+ *              		},
+ *              		"account_business": {
+ *                  		"account_business_id": 363103,
+ *                  		"account_id": "Xr82w"
+ *              		}
+ *           		}
+ *          	]
+ * 			}
+ *      }'
+ * @returns BusinessMembership object
  */
 export function transformBusinessMembershipJSON(
 	json: string
@@ -16,16 +51,48 @@ export function transformBusinessMembershipJSON(
 	const { id, role, business } = JSON.parse(json)
 
 	return {
-		id,
+		id: id.toString(),
 		role,
-		business,
+		business: transformBusinessResponse(business),
 	}
 }
 
 /**
  * Format a BusinessMembership response object
  * @param data BusinessMembership object
- * eg: { id: '1', "role": "owner" }
+ * eg: {
+ * 			"id": 168372,
+ * 			"role": "owner",
+ * 			"business": {
+ *          	"id": 77128,
+ *          	"name": "BillSpring",
+ *          	"account_id": "zDmNq",
+ *          	"address": {
+ *              	"id": 74595,
+ *              	"street": "123",
+ *              	"city": "Toronto",
+ *              	"province": "Ontario",
+ *              	"country": "Canada",
+ *              	"postal_code": "A1B2C3"
+ *          	},
+ *          	"phone_number": null,
+ *          	"business_clients": [
+ *           		{
+ *              		"id": 22347,
+ *              		"business_id": 77128,
+ *              		"account_id": "Xr82w",
+ *              		"userid": 74353,
+ *              		"client_business": {
+ *                  		"business_id": 77128
+ *              		},
+ *              		"account_business": {
+ *                  		"account_business_id": 363103,
+ *                  		"account_id": "Xr82w"
+ *              		}
+ *           		}
+ *          	]
+ * 			}
+ *      }
  * @returns BusinessMembership object
  */
 export function transformBusinessMembershipResponse(
@@ -33,8 +100,8 @@ export function transformBusinessMembershipResponse(
 ): BusinessMembership {
 	const { id, role, business } = data
 	return {
-		id,
+		id: id.toString(),
 		role,
-		business,
+		business: transformBusinessResponse(business),
 	}
 }
