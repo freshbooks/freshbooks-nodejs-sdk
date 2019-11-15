8631fb790e5847d25c719408516db2f0e8432a25
Nedu Robert <nedumrobert@gmail.com>
2019-11-15 11:13:36 -0500

    feat: added Money model and tests

---
+++ packages/api/__tests__/Money.test.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { transformMoneyJSON, transformMoneyResponse } from '../src/models/Money'
+
+describe('@freshbooks/api', () => {
+	describe('Money', () => {
+		test('Verify JSON -> model transform', async () => {
+			const json = `{
+                "amount": "1234.00",
+                "code": "USD"
+            }`
+			const model = transformMoneyJSON(json)
+
+			expect(model).toEqual(
+				expect.objectContaining({
+					amount: 1234.0,
+					code: 'USD',
+				})
+			)
+		})
+		test('Verify parsed JSON -> model transform', async () => {
+			const data = {
+				amount: '1234.00',
+				code: 'USD',
+			}
+			const model = transformMoneyResponse(data)
+
+			expect(model).toEqual(
+				expect.objectContaining({
+					amount: 1234.0,
+					code: 'USD',
+				})
+			)
+		})
+	})
+})
