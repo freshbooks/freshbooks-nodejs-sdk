6f0d5bd31624fbc8a5f292f3ced551b8bbba92e6
Bhaskar Murthy <bhaskar@secretmission.io>
2019-11-15 09:53:49 -0500

    Add pagination jsdoc

--- packages/api/src/models/Pagination.ts
+++ packages/api/src/models/Pagination.ts
@@ -1,3 +1,11 @@
+/**
+ * Pagination response
+ *
+ * @page The current page number
+ * @pages The total number of pages
+ * @total The total number of items
+ * @size Number of items per page
+ */
 export default interface Pagination {
 	page: number
 	pages: number
