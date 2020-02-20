e95a8927590d04e3450982c0e50dcd01e20d3ad5
Bhaskar Murthy <bhaskar@secretmission.io>
2020-02-19 20:21:24 -0500

    Bug fixes (#18)

--- packages/api/src/models/index.ts
+++ packages/api/src/models/index.ts
@@ -1,6 +1,7 @@
 export { default as User } from './User'
 export { default as Client } from './Client'
 export { default as Invoice } from './Invoices'
+export { default as Payment } from './Payment'
 export { default as Item } from './Item'
 export { default as Error } from './Error'
 export { default as Pagination } from './Pagination'
