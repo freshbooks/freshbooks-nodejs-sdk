0be0d097f2cf5594a542e1e19d8cb75de33bfee2
Andrew McIntosh <amcintosh@freshbooks.com>
2022-10-04 11:50:04 -0400

    🔥 Remove events package
    It doesn't do anything and there are no plans to build this out right now.

--- packages/events/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as EventAdapter, EventAdapterOptions } from './EventAdapter'
--- packages/events/src/logger.ts
+++
@@ -1,9 +0,0 @@
-import winston from 'winston'
-
-export default winston.createLogger({
-	transports: [
-		new winston.transports.Console({
-			level: 'debug',
-		}),
-	],
-})
