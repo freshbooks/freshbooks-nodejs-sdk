ab0fdc8fafe47e015df72248a0053791149da965
Brian McFadden <brian.mcfadden@freshbooks.com>
2022-10-21 11:22:09 -0700

    Create SubAccount
    See Issue #401

---
+++ packages/api/src/models/SubAccount.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { DateFormat, transformDateResponse } from './Date'
+
+export default interface SubAccount {
+    accountSubName: string
+    accountSubNumber: string
+    accountType: string
+    balance: string
+    createdAt: Date
+    currencyCode: string
+    custom: boolean
+    id: number
+    parentId: number
+    subAccountId: number
+    transactionPosted: boolean
+}
+
+export function transformSubAccountResponse(account: any): SubAccount {
+    return {
+        accountSubName: account.account_sub_name,
+        accountSubNumber: account.account_sub_number,
+        accountType: account.account_type,
+        balance: account.balance,
+        createdAt: account.created_at && transformDateResponse(account.created_at, DateFormat['YYYY-MM-DD hh:mm:ss']),
+        currencyCode: account.currency_code,
+        custom: account.custom,
+        id: account.id,
+        parentId: account.parentid,
+        subAccountId: account.sub_accountid,
+        transactionPosted: account.transaction_posted,
+    }
+}
