45e48344cd5de848d81fddf780aa6c277fe369a1
Brian McFadden <brian.mcfadden@freshbooks.com>
2022-10-30 16:46:12 -0400

    Create Detail
    See Issue #401

---
+++ packages/api/src/models/Detail.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { DateFormat, transformDateResponse } from './Date'
+
+export default interface Detail {
+    categoryId?: number
+    credit?: number
+    currencyCode?: string
+    debit?: number
+    description?: string
+    detailId?: number
+    id?: number
+    name?: string
+    subAccountId: number
+    userEnteredDate?: Date
+}
+
+export function transformDetailResponse(detail: any): Detail {
+    return {
+        categoryId: detail.categoryid,
+        credit: Number(detail.credit),
+        currencyCode: detail.currency_code,
+        debit: Number(detail.debit),
+        description: detail.description,
+        detailId: detail.detailid,
+        id: detail.id,
+        name: detail.name,
+        subAccountId: detail.sub_accountid,
+        userEnteredDate: detail.user_entered_date && transformDateResponse(detail.user_entered_date, DateFormat['YYYY-MM-DD'])
+    }
+
+}
+
+export function transformDetailRequest(detail: Detail): any {
+    return {
+        credit: detail.credit,
+        debit: detail.debit,
+        sub_accountid: detail.subAccountId,
+    }
+}
