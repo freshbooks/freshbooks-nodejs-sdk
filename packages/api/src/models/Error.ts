72b4fefa1e665ead5f2418f5cb17fe7ca0d385c7
Brian McFadden <brian.mcfadden@freshbooks.com>
2022-11-25 18:12:23 -0800

    Update error detection for Projects
    See Issue #450

--- packages/api/src/models/Error.ts
+++ packages/api/src/models/Error.ts
@@ -31,7 +31,7 @@ export default class APIClientError extends Error {
 export const isAccountingErrorResponse = ({ error, error_type: errorType, response }: any): any =>
 	error || errorType || response.errors
 
-export const isProjectErrorResponse = ({ error, error_type: errorType }: any): any => error || errorType
+export const isProjectErrorResponse = ({ error, error_type: errorType, message }: any): any => error || errorType || message
 
 export const isEventErrorResponse = ({ errno, message }: any): any => errno || message
 
