dda23ffb45a9f7c6e0431bbea58feab5b537dcb2
Brian McFadden <brian.mcfadden@freshbooks.com>
2022-12-13 13:12:17 -0800

    Refactor transformBusinessResponse

--- packages/api/src/models/Business.ts
+++ packages/api/src/models/Business.ts
@@ -22,57 +22,17 @@ export interface BusinessResponse {
 	business_clients: BusinessClientResponse[]
 }
 
-/**
- * Format a Business response object
- * @param data Business object
- * eg: {
- *          "id": 77128,
- *          "name": "BillSpring",
- *          "account_id": "zDmNq",
- *          "address": {
- *              "id": 74595,
- *              "street": "123",
- *              "city": "Toronto",
- *              "province": "Ontario",
- *              "country": "Canada",
- *              "postal_code": "A1B2C3"
- *          },
- *          "phone_number": null,
- *          "business_clients": [
- *           {
- *              "id": 22347,
- *              "business_id": 77128,
- *              "account_id": "Xr82w",
- *              "userid": 74353,
- *              "client_business": {
- *                  "business_id": 77128
- *              },
- *              "account_business": {
- *                  "account_business_id": 363103,
- *                  "account_id": "Xr82w"
- *              }
- *           }
- *          ]
- *      }
- * @returns Business object
- */
-export function transformBusinessResponse({
-	id,
-	name,
-	account_id: accountId = '',
-	address,
-	phone_number,
-	business_clients,
-}: BusinessResponse): Business {
+export function transformBusinessResponse(business: BusinessResponse): Business {
 	return {
-		id: id,
-		name,
-		accountId: accountId !== null ? accountId.toString() : '',
-		address: address !== null ? transformAddressParsedResponse(address) : null,
-		phoneNumber: phone_number !== null ? transformPhoneNumberResponse(phone_number) : null,
-		businessClients: business_clients.map(transformBusinessClientResponse),
+		id: business.id,
+		name: business.name,
+		accountId: business.account_id !== null ? business.account_id.toString() : '',
+		address: business.address !== null ? transformAddressParsedResponse(business.address) : null,
+		phoneNumber: business.phone_number !== null ? transformPhoneNumberResponse(business.phone_number) : null,
+		businessClients: business.business_clients.map(transformBusinessClientResponse),
 	}
 }
+
 /**
  * Parse a JSON string to @Business object
  * @param json JSON string
