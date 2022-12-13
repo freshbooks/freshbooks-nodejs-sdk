84b15e3e9b9633f9ec6fabb1b509881cc3f9fc50
Brian McFadden <brian.mcfadden@freshbooks.com>
2022-12-13 11:13:30 -0800

    Refactor AccountBusiness transform response method

--- packages/api/src/models/AccountBusiness.ts
+++ packages/api/src/models/AccountBusiness.ts
@@ -9,31 +9,13 @@ export interface AccountBusinessResponse {
 	account_business_id: number
 }
 
-/**
- * Format a AccountBusiness response object
- * @param data Account business object
- * eg: { "account_business_id": 363103, "account_id": "Xr82w" }
- * @returns Account business object
- */
-export function transformAccountBusinessResponse({
-	account_id,
-	account_business_id,
-}: AccountBusinessResponse): AccountBusiness {
+export function transformAccountBusinessResponse(accountBusiness: AccountBusinessResponse): AccountBusiness {
 	return {
-		accountId: account_id,
-		businessId: Number(account_business_id),
+		accountId: accountBusiness.account_id,
+		businessId: Number(accountBusiness.account_business_id),
 	}
 }
 
-/**
- * Parse a JSON string to @AccountBusiness object
- * @param json JSON string
- * eg: '{
- *          "account_business_id": 363103,
- *          "account_id": "Xr82w"
- *      }'
- * @returns AccountBusiness object
- */
 export function transformAccountBusinessJSON(json: string): AccountBusiness {
 	const response: AccountBusinessResponse = JSON.parse(json)
 	return transformAccountBusinessResponse(response)
