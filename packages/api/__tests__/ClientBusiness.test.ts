4b657632f4006789b8c9cd0d37a82427129b3562
Brian McFadden <brian.mcfadden@freshbooks.com>
2022-12-14 09:12:43 -0800

    Delete transformClientBusinessJSON

--- packages/api/__tests__/ClientBusiness.test.ts
+++ packages/api/__tests__/ClientBusiness.test.ts
@@ -1,20 +1,8 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import { transformClientBusinessJSON, transformClientBusinessResponse } from '../src/models/ClientBusiness'
+import { transformClientBusinessResponse } from '../src/models/ClientBusiness'
 
 describe('@freshbooks/api', () => {
 	describe('Client Business', () => {
-		test('Verify JSON -> model transform', async () => {
-			const json = `{
-                "business_id": 77128
-            }`
-			const model = transformClientBusinessJSON(json)
-
-			expect(model).toEqual(
-				expect.objectContaining({
-					businessId: 77128,
-				})
-			)
-		})
 		test('Verify parsed JSON -> model transform', async () => {
 			const data = {
 				business_id: 77128,
