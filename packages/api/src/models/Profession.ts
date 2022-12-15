5d9c96efaf3bf912e700111a5464abe84a21d012
Brian McFadden <brian.mcfadden@freshbooks.com>
2022-12-14 16:04:40 -0800

    Refactor transformProfessionResponse

--- packages/api/src/models/Profession.ts
+++ packages/api/src/models/Profession.ts
@@ -17,24 +17,12 @@ interface ProfessionResponse {
 	designation: Nullable<string>
 }
 
-/**
- * Format an Profession response object
- * @param data Profession object
- * eg: {"id": 17748, "title": "Accounting", "company": "BillSpring", "designation": null, "business_id": 2122866}
- * @returns Profession object
- */
-export function transformProfessionResponse({
-	id,
-	title,
-	company,
-	designation,
-	business_id,
-}: ProfessionResponse): Profession {
+export function transformProfessionResponse(profession: ProfessionResponse): Profession {
 	return {
-		id: id.toString(),
-		title,
-		company,
-		designation,
-		businessId: business_id !== null ? business_id.toString() : null,
+		id: profession.id.toString(),
+		title: profession.title,
+		company: profession.company,
+		designation: profession.designation,
+		businessId: profession.business_id !== null ? profession.business_id.toString() : null,
 	}
 }
