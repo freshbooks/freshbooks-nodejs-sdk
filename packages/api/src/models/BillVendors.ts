f134daae5b7eaa896b2edfbbc52c6500db399d98
Brian McFadden <brian.mcfadden@freshbooks.com>
2022-12-15 11:47:32 -0800

    Rename BillVendors method params

--- packages/api/src/models/BillVendors.ts
+++ packages/api/src/models/BillVendors.ts
@@ -65,66 +65,66 @@ export function transformBillVendorsListResponse(data: string): { bill_vendors: 
 	}
 }
 
-export function transformBillVendorsParsedResponse(billVendor: any): BillVendors {
+export function transformBillVendorsParsedResponse(vendor: any): BillVendors {
 	return {
-		accountNumber: billVendor.account_number,
-		city: billVendor.city,
-		country: billVendor.country,
-		currencyCode: billVendor.currency_code,
-		is1099: billVendor.is_1099,
-		language: billVendor.language,
-		note: billVendor.note,
+		accountNumber: vendor.account_number,
+		city: vendor.city,
+		country: vendor.country,
+		currencyCode: vendor.currency_code,
+		is1099: vendor.is_1099,
+		language: vendor.language,
+		note: vendor.note,
 		outstandingBalance: 
-			billVendor.outstanding_balance && 
-			billVendor.outstanding_balance.map((balance: any): Money => transformMoneyParsedResponse(balance)),
+			vendor.outstanding_balance && 
+			vendor.outstanding_balance.map((balance: any): Money => transformMoneyParsedResponse(balance)),
 		overdueBalance: 
-			billVendor.overdue_balance && 
-			billVendor.overdue_balance.map((balance: any): Money => transformMoneyParsedResponse(balance)),
-		phone: billVendor.phone,
-		postalCode: billVendor.postal_code,
-		primaryContactEmail: billVendor.primary_contact_email,
-		primaryContactFirstName: billVendor.primary_contact_first_name,
-		primaryContactLastName: billVendor.primary_contact_last_name,
-		province: billVendor.province,
-		street: billVendor.street,
-		street2: billVendor.street2,
+			vendor.overdue_balance && 
+			vendor.overdue_balance.map((balance: any): Money => transformMoneyParsedResponse(balance)),
+		phone: vendor.phone,
+		postalCode: vendor.postal_code,
+		primaryContactEmail: vendor.primary_contact_email,
+		primaryContactFirstName: vendor.primary_contact_first_name,
+		primaryContactLastName: vendor.primary_contact_last_name,
+		province: vendor.province,
+		street: vendor.street,
+		street2: vendor.street2,
 		taxDefaults: 
-			billVendor.tax_defaults && 
-			billVendor.tax_defaults.map((billVendorTax: any): BillVendorTax => transformBillVendorTaxParsedResponse(billVendorTax)),
-		vendorId: billVendor.vendorid,
-		visState: billVendor.vis_state,
-		website: billVendor.website,
-		vendorName: billVendor.vendor_name,
-		createdAt: billVendor.created_at && transformDateResponse(billVendor.created_at, DateFormat['YYYY-MM-DD hh:mm:ss']),
-		updatedAt: billVendor.updated_at && transformDateResponse(billVendor.updated_at, DateFormat['YYYY-MM-DD hh:mm:ss']),
+			vendor.tax_defaults && 
+			vendor.tax_defaults.map((tax: any): BillVendorTax => transformBillVendorTaxParsedResponse(tax)),
+		vendorId: vendor.vendorid,
+		visState: vendor.vis_state,
+		website: vendor.website,
+		vendorName: vendor.vendor_name,
+		createdAt: vendor.created_at && transformDateResponse(vendor.created_at, DateFormat['YYYY-MM-DD hh:mm:ss']),
+		updatedAt: vendor.updated_at && transformDateResponse(vendor.updated_at, DateFormat['YYYY-MM-DD hh:mm:ss']),
 	}
 }
 
-export function transformBillVendorsRequest(billVendor: BillVendors): string {
+export function transformBillVendorsRequest(vendor: BillVendors): string {
 	return JSON.stringify({
 		bill_vendor: {
-			account_number: billVendor.accountNumber,
-			city: billVendor.city,
-			country: billVendor.country,
-			currency_code: billVendor.currencyCode,
-			is_1099: billVendor.is1099,
-			language: billVendor.language,
-			note: billVendor.note,
-			phone: billVendor.phone,
-			postal_code: billVendor.postalCode,
-			primary_contact_email: billVendor.primaryContactEmail,
-			primary_contact_first_name: billVendor.primaryContactFirstName,
-			primary_contact_last_name: billVendor.primaryContactLastName,
-			province: billVendor.province,
-			street: billVendor.street,
-			street2: billVendor.street2,
+			account_number: vendor.accountNumber,
+			city: vendor.city,
+			country: vendor.country,
+			currency_code: vendor.currencyCode,
+			is_1099: vendor.is1099,
+			language: vendor.language,
+			note: vendor.note,
+			phone: vendor.phone,
+			postal_code: vendor.postalCode,
+			primary_contact_email: vendor.primaryContactEmail,
+			primary_contact_first_name: vendor.primaryContactFirstName,
+			primary_contact_last_name: vendor.primaryContactLastName,
+			province: vendor.province,
+			street: vendor.street,
+			street2: vendor.street2,
 			tax_defaults:
-				billVendor.taxDefaults &&
-				billVendor.taxDefaults.map((billVendorTax: any): BillVendorTax => transformBillVendorTaxRequest(billVendorTax)),
-			vendorid: billVendor.vendorId,
-			vis_state: billVendor.visState,
-			website: billVendor.website,
-			vendor_name: billVendor.vendorName,
+				vendor.taxDefaults &&
+				vendor.taxDefaults.map((tax: any): BillVendorTax => transformBillVendorTaxRequest(tax)),
+			vendorid: vendor.vendorId,
+			vis_state: vendor.visState,
+			website: vendor.website,
+			vendor_name: vendor.vendorName,
 		},
 	})
 }
