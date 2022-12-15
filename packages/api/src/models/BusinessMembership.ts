952bb095d5db12bbbc25b98364f45c30205fe12c
Brian McFadden <brian.mcfadden@freshbooks.com>
2022-12-15 11:52:48 -0800

    Add eslint disable comment

--- packages/api/src/models/BusinessMembership.ts
+++ packages/api/src/models/BusinessMembership.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import Business, { transformBusinessParsedResponse, BusinessResponse } from './Business'
 
 export default interface BusinessMembership {
